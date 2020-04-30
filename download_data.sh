47a965849507f178e298371596abee3dbd4324d2
Paras Jain <parasj96@gmail.com>
2020-04-30 15:09:01 -0700

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p data
-(cd data; wget -nc https://misc-upload-parasjain.s3.amazonaws.com/data/contrastive-js-representation/js150k_eval.pkl)
-(cd data; wget -nc https://misc-upload-parasjain.s3.amazonaws.com/data/contrastive-js-representation/js150k_train.pkl)
+rsync -avhW --no-compress --progress  /work/paras/data/js150k/data.tar.gz ./data/
+#(cd data; wget -nc https://misc-upload-parasjain.s3.amazonaws.com/data/contrastive-js-representation/js150k_eval.pkl)
+#(cd data; wget -nc https://misc-upload-parasjain.s3.amazonaws.com/data/contrastive-js-representation/js150k_train.pkl)
 (cd data; tar -xvzf data.tar.gz)
