597e35432f08828bc5c064e24b8ffaf1b29fed18
Paras Jain <parasj96@gmail.com>
2020-05-01 12:12:32 -0700

    Load data from EECS homedir if not on local file-system

--- download_data.sh
+++ download_data.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 mkdir -p data
-rsync -avhW --no-compress --progress  /work/paras/data/js150k/data.tar.gz ./data/
-#(cd data; wget -nc https://misc-upload-parasjain.s3.amazonaws.com/data/contrastive-js-representation/js150k_eval.pkl)
-#(cd data; wget -nc https://misc-upload-parasjain.s3.amazonaws.com/data/contrastive-js-representation/js150k_train.pkl)
-(cd data; tar -xvzf data.tar.gz)
+
+LOCAL_JS150k=/work/paras/data/js150k/data.tar.gz
+REMOTE_JS150k=https://people.eecs.berkeley.edu/~paras/datasets/js150k/data.tar.gz
+if [ -f "$LOCAL_SHARED_JS150k" ]; then
+    rsync -avhW --no-compress --progress $LOCAL_SHARED_JS150k ./data/
+else 
+    (cd data; wget -nc -O ./data/data.tar.gz $REMOTE_JS150k)
+fi
+
+(cd data; tar -xzf data.tar.gz)
