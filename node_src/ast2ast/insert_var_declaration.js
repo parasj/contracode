45ceee290387d0c0897d5017ade24d1b61308b85
Tianjun <tianjunz@berkeley.edu>
2020-05-03 16:11:54 -0700

    tested insert_var_decl and rename_var

--- node_src/ast2ast/insert_var_declaration.js
+++ node_src/ast2ast/insert_var_declaration.js
@@ -5,8 +5,9 @@ const generate = require('@babel/generator').default;
 const t = require("@babel/types");
 const prettier = require('prettier');
 
-const js_src = fs.readFileSync(0).toString(); // STDIN_FILENO = 0
+const js_src = fs.readFileSync(process.argv[2]).toString(); // STDIN_FILENO = 0
 const ast = parser(js_src, {sourceType: 'module'});
+// console.log(js_src);
 
 /*
 Function signature for transformations:
@@ -14,13 +15,17 @@ function(ASTNode, {optional_arguments}) -> List[AST]
 Transformations are applied via a flatMap
 */
 
-function insert_var_declaration(ast, {prob}) {
-    FunctionDeclaration(path) {
-        if (Math.random() < prob) {
-            const id = path.scope.generateUidIdentifierBasedOnNode(path.node.id);
-            path.get('body').unshiftContainer('body', t.variableDeclaration("var", [t.variableDeclarator(id)]));
-    }
+function insert_var_declaration(ast, prob) {
+    traverse(ast, {
+        FunctionDeclaration(path) {
+            if (Math.random() < prob) {
+                const id = path.scope.generateUidIdentifierBasedOnNode(path.node.id);
+                path.get('body').unshiftContainer('body', t.variableDeclaration("var", [t.variableDeclarator(id)]));
+            }
+        }
+    });
     return ast;
 }
 
+// console.log(generate(insert_var_declaration(ast, 1)).code);
 module.exports = insert_var_declaration;
--- node_src/ast2ast/rename_variable.js
+++ node_src/ast2ast/rename_variable.js
@@ -5,8 +5,9 @@ const generate = require('@babel/generator').default;
 const t = require("@babel/types");
 const prettier = require('prettier');
 
-const js_src = fs.readFileSync(0).toString(); // STDIN_FILENO = 0
-const ast = parser(js_src, {sourceType: 'module'});
+const js_src = fs.readFileSync(process.argv[2]).toString(); // STDIN_FILENO = 0
+const ast = parser(js_src);
+// console.log(js_src)
 
 /*
 Function signature for transformations:
@@ -16,7 +17,7 @@ function(ASTNode, {prob}) -> ASTNode
 prob is the probability to replace the name with fixed name
 */
 
-function rename_variable(ast, {prob}) {
+function rename_variable(ast, prob) {
     traverse(ast, {
         FunctionDeclaration(path) {
             if (Math.random() < prob) {
@@ -24,8 +25,9 @@ function rename_variable(ast, {prob}) {
                 path.scope.rename(exampleState);
             }
         }
-    }
+    });
     return ast;
 }
 
+// console.log(generate(rename_variable(ast, 1)).code);
 module.exports = rename_variable;
