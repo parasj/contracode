1be97db5b9770c998287f351910533178db553c5
Ajay Jain <ajayj@berkeley.edu>
2020-05-05 22:41:45 -0700

    Add jsonl filtering with multiple files

--- scripts/filter_jsonl.py
+++ scripts/filter_jsonl.py
@@ -57,7 +57,48 @@ def filter_dataset(path: str, out_path: str, require_fields=[]):
     f.close()
 
 
+def filter_datasets(paths, out_path: str, require_fields=[]):
+    logger.debug(f"Requiring fields {require_fields}")
+    total_lines = 0
+    examples = []
+    for path in paths:
+        full_path = pathlib.Path(path).resolve()
+        f = gzip.open(full_path, "rb") if path.endswith(".jsonl.gz") else full_path.open("r")
+        reader = jsonlines.Reader(f)
+
+        logger.debug(f"Loading {full_path}")
+        for json_dict in tqdm.tqdm(reader, desc=full_path.name):
+            total_lines += 1
+            # Check all required fields are present
+            if any([field not in json_dict or not json_dict[field] for field in require_fields]):
+                continue
+
+            # We need the identifier (method name) as a label. Filter invalid identifiers
+            if 'identifier' in require_fields and _valid_identifier_regex.match(json_dict['identifier']) == None:
+                print(f"WARN: Invalid identifier {require_fields['identifier']}, skipping record")
+                continue
+
+            examples.append(json_dict)
+            if total_lines % 100000 == 0:
+                logger.debug(f"Filtered jsonl to {len(examples)}/{total_lines}")
+        f.close()
+
+        logger.debug(f"DONE: Filtered jsonl to {len(examples)}/{total_lines}")
+
+    # TODO: Subsample
+    
+    # Write output
+    full_out_path = pathlib.Path(out_path).resolve()
+    f = gzip.open(full_out_path, "wb") if out_path.endswith(".jsonl.gz") else full_out_path.open("w")
+    writer = jsonlines.Writer(f)
+    logger.debug(f"Writing output to {full_out_path}...")
+    writer.write_all(examples)
+    logger.debug(f"DONE writing")
+    f.close()
+
+
 if __name__ == "__main__":
     fire.Fire({
         "filter_dataset": filter_dataset,
+        "filter_datasets": filter_datasets
     })
