616237a4c8380ede6aeb217c5dfc41a696d8a9cc
Ajay Jain <ajayj@berkeley.edu>
2020-05-09 23:31:33 -0700

    Add transform_jsonl.js

---
+++ node_src/transform_jsonl.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const lineReader = require('line-reader');
+
+var JavascriptAugmentations = require('./javascript_augmentations')
+const javascriptAugmenter = new JavascriptAugmentations();
+
+
+function escapeRegExp(string) {
+    return string.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+  }
+
+
+function _fix_json_dict(json_dict, src_function_key, src_method_name_key) {
+    // Fix cropped "function" token at the begging of the function string
+    // for (var regex of _fix_function_crop_regexes) {
+    //     json_dict[src_function_key] = regex.sub(r'function\1', json_dict[src_function_key], count=1);
+    // }
+    const method_name = json_dict[src_method_name_key];
+    if (method_name) {
+        // Remove function name from declaration, but leave it in the function body
+        const _function_name_regex = new RegExp(`(function\\s*)${escapeRegExp(method_name)}(\\s*\\()`, '');
+        json_dict[src_function_key] = json_dict[src_function_key].replace(_function_name_regex, '$1x$2');
+    } else {
+        json_dict[src_function_key] = "const x = " + json_dict[src_function_key];
+    }
+}
+
+
+const augmentations = [	
+    {"fn": "rename_variable"},
+    {"fn": "insert_var_declaration"},
+    {"fn": "terser"},
+    {"fn": "sample_lines"}
+]
+
+const numAlternatives = 20;
+
+var outFilepath = '/home/ajay/coderep/representjs/data/codesearchnet_javascript/javascript_dedupe_definitions_nonoverlap_v2_train_augmented.jsonl';
+var writeStream = fs.createWriteStream(outFilepath)
+var numProcessed = 0;
+var startTime = Date.now();
+
+lineReader.eachLine('/dev/shm/ajay/javascript_dedupe_definitions_nonoverlap_v2_train.jsonl', function(line, last) {
+    var data = JSON.parse(line);
+    _fix_json_dict(data, 'function', 'identifier');
+    const identifier = data['identifier'];
+    const fn = data['function'];
+
+    let alternatives = [fn];
+    for (let i = 0; i < numAlternatives; i++) {
+        const transformed = javascriptAugmenter.transform(fn, augmentations);
+        alternatives.push(transformed);
+    }
+    let alternativesStr = JSON.stringify(alternatives);
+    writeStream.write(alternativesStr + "\n")
+
+    numProcessed++;
+
+    if (numProcessed % 1000 == 0) {
+        var elapsed = Date.now() - startTime;
+        var linesPerSecond = numProcessed / (elapsed / 1000);
+        var remaining = (1843099 - numProcessed) / linesPerSecond / 60;
+        console.log(`Processed ${numProcessed} lines (${linesPerSecond} per sec, ${remaining} minutes remaining)`)
+    }
+
+    if (last) {
+        console.log("Done reading!");
+
+        return false;
+    }
+});
