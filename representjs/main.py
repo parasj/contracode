b1bccb9f96c2bb8a5585792e30651141260086d7
Ajay Jain <ajayj@berkeley.edu>
2020-05-11 23:16:17 -0700

    Add checkpoint load log

--- representjs/main.py
+++ representjs/main.py
@@ -194,6 +194,7 @@ def train(
             # TODO: Try loading encoder_k -- has ema on parameters
             if key.startswith('encoder_k.') and 'project_layer' not in key:
                 remapped_key = key[len('encoder_k.'):]
+                logger.debug(f"Remapping checkpoint key {key} to {remapped_key}. Value mean: {value.mean().item()}")
                 encoder_state_dict[remapped_key] = value
         model.encoder.load_state_dict(encoder_state_dict)
         logger.info(f"Loaded state dict from {resume_path}")
