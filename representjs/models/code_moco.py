bef058ad82ac674b0e61c4be6e9678bf54d60218
Paras Jain <parasj96@gmail.com>
2020-05-14 14:57:27 -0700

    Import dependency

--- representjs/models/code_moco.py
+++ representjs/models/code_moco.py
@@ -57,53 +57,6 @@ class MoCoTemplate(nn.Module):
 
         self.queue_ptr[0] = ptr
 
-    # @torch.no_grad()
-    # def _batch_shuffle_ddp(self, x):
-    #     """
-    #     Batch shuffle, for making use of BatchNorm.
-    #     *** Only support DistributedDataParallel (DDP) model. ***
-    #     """
-    #     # gather from all gpus
-    #     batch_size_this = x.shape[0]
-    #     x_gather = concat_all_gather(x)
-    #     batch_size_all = x_gather.shape[0]
-    #
-    #     num_gpus = batch_size_all // batch_size_this
-    #
-    #     # random shuffle index
-    #     idx_shuffle = torch.randperm(batch_size_all).cuda()
-    #
-    #     # broadcast to all gpus
-    #     torch.distributed.broadcast(idx_shuffle, src=0)
-    #
-    #     # index for restoring
-    #     idx_unshuffle = torch.argsort(idx_shuffle)
-    #
-    #     # shuffled index for this gpu
-    #     gpu_idx = torch.distributed.get_rank()
-    #     idx_this = idx_shuffle.view(num_gpus, -1)[gpu_idx]
-    #
-    #     return x_gather[idx_this], idx_unshuffle
-    #
-    # @torch.no_grad()
-    # def _batch_unshuffle_ddp(self, x, idx_unshuffle):
-    #     """
-    #     Undo batch shuffle.
-    #     *** Only support DistributedDataParallel (DDP) model. ***
-    #     """
-    #     # gather from all gpus
-    #     batch_size_this = x.shape[0]
-    #     x_gather = concat_all_gather(x)
-    #     batch_size_all = x_gather.shape[0]
-    #
-    #     num_gpus = batch_size_all // batch_size_this
-    #
-    #     # restored index for this gpu
-    #     gpu_idx = torch.distributed.get_rank()
-    #     idx_this = idx_unshuffle.view(num_gpus, -1)[gpu_idx]
-    #
-    #     return x_gather[idx_this]
-
     def forward(self, im_q, im_k):
         """
         Input:
