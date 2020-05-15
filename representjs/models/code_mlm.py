685aeb18e702662717181af8d1b143f71e9a7d77
Paras Jain <parasj96@gmail.com>
2020-05-15 10:25:22 -0700

    Add shape asserts

--- representjs/models/code_mlm.py
+++ representjs/models/code_mlm.py
@@ -7,21 +7,18 @@ from models.encoder import CodeEncoder
 class CodeMLM(nn.Module):
     def __init__(self, n_tokens, d_model=512, pad_id=None, **encoder_args):
         super().__init__()
+        self.n_tokens = n_tokens
+        self.d_model = d_model
         self.encoder = CodeEncoder(n_tokens, project=False, pad_id=pad_id, d_model=d_model, **encoder_args)
         self.head = nn.Sequential(
             nn.Linear(d_model, d_model),
             nn.ReLU(),
-            nn.LayerNorm(d_model)
-        )
+            nn.LayerNorm(d_model))
 
     def forward(self, im):
-        """
-        Input:
-            im: a batch of query images
-        Output:
-            logits
-        """
-        features = self.encoder(im)  # LxBxD
-        features = self.head(features)  # LxBxD
-        logits = torch.matmul(features, self.encoder.embedding.weight.transpose(0, 1))  # [L, B, ntok]
-        return torch.transpose(logits, 0, 1)  # [B, T, ntok]
+        L, B, D = im.shape
+        assert D == self.d_model
+        features = self.encoder(im).view(L, B, D)  # LxBxD
+        features = self.head(features).view(L, B, D)  # LxBxD
+        logits = torch.matmul(features, self.encoder.embedding.weight.transpose(0, 1)).view(L, B, self.n_tokens)  # [L, B, ntok]
+        return torch.transpose(logits, 0, 1).view(B, L, self.n_tokens)  # [B, T, ntok]
