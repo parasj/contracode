0a343ef13ce1bf1fbcb6916cce709708ddd1473f
Ajay Jain <ajayj@berkeley.edu>
2020-05-22 15:25:19 -0700

    Add typing dataset, model, training script

---
+++ representjs/data/deeptyper_dataset.py
@@ -0,0 +1,138 @@
+import jsbeautifier
+from loguru import logger
+import torch
+import sentencepiece as spm
+
+from representjs.data.util import normalize_program
+
+
+TYPED_MARKER_START = "__LS__"
+TYPED_MARKER_END = "__LE__"
+def _tokenize(deeptyper_line, sp, target_to_id, max_length, split_source_targets_by_tab=False):
+    """Given a line from the .txt data files in DeepTyper, format and 
+    tokenize the code into subwords while retaining a mapping between
+    type labels and the subwords.
+    
+    Returns:
+        Beautified program
+        List of subword IDs
+        List of (label_id, label_start, label_end) tuples where label_start/end specify a range of subword IDs"""
+    assert TYPED_MARKER_START not in deeptyper_line
+    assert TYPED_MARKER_END not in deeptyper_line
+
+    if split_source_targets_by_tab:
+        # Code tokens and type labels are delimeted by tab, as in .json files
+        js_tokens, labels = line.split("\t")
+    else:
+        # Code tokens and type labels are delimited by space after </s>, as in .txt files
+        js_end = deeptyper_line.index("</s>") + len("</s>")
+        js_tokens = deeptyper_line[:js_end]
+        labels = deeptyper_line[js_end+1:]
+
+    # Split code by spaces to get DeepTyper tokens, excluding <s>, </s>
+    js_tokens = js_tokens.split(" ")[1:-1]
+    labels = labels.split(" ")[1:-1]
+    assert len(js_tokens) == len(labels)
+
+    # Add markers to each labeled token: tokens without no-type label
+    js_tokens_with_markers = []
+    for tok, label in zip(js_tokens, labels):
+        if label != "O":
+            tok = f"{TYPED_MARKER_START}{tok}{TYPED_MARKER_END}" 
+        js_tokens_with_markers.append(tok)
+
+    # Normalize program by beautifying
+    js_beautified = jsbeautifier.beautify(" ".join(js_tokens_with_markers))
+    js_beautified_norm = normalize_program(js_beautified)
+    js_beautified_norm = js_beautified_norm
+    
+    # Subword tokenize, separately tokenizing each marked identifier
+    js_buffer = js_beautified_norm
+    subword_ids = [sp.PieceToId("<s>")]
+    label_segments = []
+
+    last_identifier_end = 0
+    start = js_buffer.find(TYPED_MARKER_START)
+    labels = list(filter(lambda l: l != "O", labels))
+    label_i = 0
+    if start < 0:
+        # No labeled words in this text, just tokenize the full program
+        buffer_ids = sp.EncodeAsIds(js_buffer)
+        subword_ids.extend(buffer_ids[:max_length-2])
+    while start >= 0:
+        # buffer: "stuff [start ptr]__LS__typedIdentifier__LE__..."
+        # Tokenize stuff before the typed identifier
+        buffer_ids = sp.EncodeAsIds(js_buffer[last_identifier_end:start])
+        if len(subword_ids) + len(buffer_ids) + 1 > max_length:  # +1 for </s>
+            break
+        subword_ids.extend(buffer_ids)
+
+        # Tokenize typed identifier and record label
+        start = start + len(TYPED_MARKER_START)
+        end = js_buffer.index(TYPED_MARKER_END, start)
+        assert end > start, "Zero-length identifier"
+        identifier = js_buffer[start:end]
+        identifier_ids = sp.EncodeAsIds(identifier)
+        if len(subword_ids) + len(identifier_ids) + 1 > max_length:  # +1 for </s>
+            break
+        # A segment is (label_id, label_start, label_end)
+        label_segments.append((target_to_id[labels[label_i]], len(subword_ids), len(subword_ids) + len(identifier_ids)))
+        subword_ids.extend(identifier_ids)
+
+        start = js_buffer.find(TYPED_MARKER_START, start + 1)
+        last_identifier_end = end + len(TYPED_MARKER_END)
+        label_i += 1
+    
+    subword_ids.append(sp.PieceToId("</s>"))
+    assert subword_ids[0] == sp.PieceToId("<s>")
+    assert subword_ids[-1] == sp.PieceToId("</s>")
+
+    return js_beautified, subword_ids, label_segments
+
+
+def load_type_vocab(vocab_path):
+    """Make type (target) vocab. vocab_path should contain have one type on each line."""
+    id_to_target = {}
+    target_to_id = {}
+    with open(vocab_path, "r") as f:
+        for i, line in enumerate(f):
+            assert line
+            tok = line.strip()
+            assert tok
+            id_to_target[i] = tok
+            target_to_id[tok] = i
+    assert len(id_to_target) == len(target_to_id)
+    print(f"Loaded vocab from {vocab_path} with {len(id_to_target)} items")
+    return id_to_target, target_to_id
+
+
+class DeepTyperDataset(torch.utils.data.Dataset):
+    def __init__(self, data_path, type_vocab_path, sentencepiece_filepath, max_length=1024, subword_regularization_alpha=0.):
+        assert subword_regularization_alpha == 0.
+        self.max_length = max_length
+        self.subword_regularization_alpha = subword_regularization_alpha
+
+        self.sp = spm.SentencePieceProcessor()
+        self.sp.Load(sentencepiece_filepath)
+
+        self.id_to_target, self.target_to_id = load_type_vocab(type_vocab_path)
+
+        self.lines = []
+        with open(data_path, "r") as f:
+            for line in f:
+                self.lines.append(line)
+        
+    def __getitem__(self, idx):
+        line = self.lines[idx]
+        _, subword_ids, label_segments = _tokenize(line, self.sp, self.target_to_id, self.max_length)
+        assert len(subword_ids) <= self.max_length
+        subword_ids = torch.tensor(subword_ids, dtype=torch.long)
+        label_segments = torch.tensor(label_segments, dtype=torch.long)
+        return (subword_ids, label_segments)
+
+    def __len__(self):
+        return len(self.lines)
+
+
+if __name__=="__main__":
+    pass
---
+++ representjs/models/typetransformer.py
@@ -0,0 +1,42 @@
+import math
+
+import torch
+import torch.nn as nn
+
+from models.code_moco import CodeEncoder
+
+
+class TypeTransformer(nn.Module):
+    def __init__(self, n_tokens, n_output_tokens, d_model=512, d_rep=128, n_head=8, n_encoder_layers=6, d_ff=2048, dropout=0.1,
+                 activation="relu", norm=True, pad_id=None):
+        super(TypeTransformer, self).__init__()
+        assert norm
+        assert pad_id is not None
+        self.config = {k: v for k, v in locals().items() if k != 'self'}
+
+        # Encoder
+        self.encoder = CodeEncoder(n_tokens, d_model, d_rep, n_head, n_encoder_layers, d_ff, dropout, activation, norm, pad_id, project=False)
+
+        # Output for type prediction
+        # TODO: Try LeakyReLU
+        self.output = nn.Sequential(nn.Linear(d_model, d_model), nn.ReLU(), nn.Linear(d_model, n_output_tokens))
+
+    def forward(self, src_tok_ids, output_attention):
+        r"""
+        Arguments:
+            src_tok_ids: [B, L] long tensor
+            output_attention: [B, L, L] float tensor
+        """
+        if src_tok_ids.size(0) != output_attention.size(0):
+            raise RuntimeError("the batch number of src_tok_ids and output_attention must be equal")
+
+        # Encode
+        memory = self.encoder(src_tok_ids)  # LxBxD
+        memory = memory.transpose(0, 1)  # BxLxD
+
+        # Aggregate features to the starting token in each labeled identifier
+        memory = torch.matmul(output_attention, memory)  # BxLxD
+
+        # Predict types
+        logits = self.output(memory)  # BxLxV
+        return logits
