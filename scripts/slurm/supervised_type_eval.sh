614d68530f0ec1860f767716e35f8d169201a891
Paras Jain <paras_jain@berkeley.edu>
2020-05-30 17:37:52 -0700

    update scripts

--- scripts/slurm/supervised_type_eval.sh
+++ scripts/slurm/supervised_type_eval.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=type_finetune
-#SBATCH --output=/home/eecs/paras/slurm/coderep/%j_supervised_type_finetune.log
+#SBATCH --job-name=type_eval
+#SBATCH --output=/home/eecs/paras/slurm/coderep/%j_supervised_type_eval.log
 #SBATCH --ntasks=1
-#SBATCH --mem=256000
-#SBATCH --time=125:00:00
+#SBATCH --mem=64000
+#SBATCH --time=4:00:00
 #SBATCH --exclude=atlas,blaze,r16
 
 # print host statistics
@@ -24,28 +24,18 @@ export DATA_CACHE="/data/paras/data_cache"
 export FREE_PORT="$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1])')";
 
 # argument parsing
-export BATCHSIZE=${BATCHSIZE:-16}
-export LR=${LR:-"1e-4"}
+export BATCHSIZE=${BATCHSIZE:-1}
 export SUBWORD_REGULARIZATION=${SUBWORD_REGULARIZATION:-0}
-export RESUME_ENCODER_NAME=${RESUME_ENCODER_NAME:-"encoder_q"}
-export MAXSEQLEN=${MAXSEQLEN:-2048}
 
 # set default arugments
-[ -z "$RUNNAME" ] && { echo "Need to set RUNNAME"; exit 1; }
 [ -z "$CKPT" ] && { echo "Need to set CKPT"; exit 1; }
 [ -z "$SUBWORD_REGULARIZATION" ] && { echo "Need to set SUBWORD_REGULARIZATION"; exit 1; }
 [ -z "$BATCHSIZE" ] && { echo "Need to set BATCHSIZE"; exit 1; }
-[ -z "$LR" ] && { echo "Need to set LR"; exit 1; }
-[ -z "$RESUME_ENCODER_NAME" ] && { echo "Need to set RESUME_ENCODER_NAME"; exit 1; }
-[ -z "$MAXSEQLEN" ] && { echo "Need to set MAXSEQLEN"; exit 1; }
 [ -f "$CKPT" ] || { echo "Checkpoint not found"; exit 1; }
 
 # print argument names
-echo "RUNNAME = $RUNNAME"
 echo "CKPT = $CKPT"
-echo "LR = $LR"
 echo "BATCHSIZE = $BATCHSIZE"
-echo "MAXSEQLEN = $MAXSEQLEN"
 echo ""
 echo "CUDA_VISIBLE_DEVICES = $CUDA_VISIBLE_DEVICES"
 echo "CUDA_DEVICE_ORDER = $CUDA_DEVICE_ORDER"
@@ -59,6 +49,7 @@ rsync -avhW --no-compress --progress /work/paras/representjs/data/codesearchnet_
 rsync -avhW --no-compress --progress /work/paras/coderep/DeepTyper/data/target_wl $DATA_CACHE
 rsync -avhW --no-compress --progress /work/paras/coderep/DeepTyper/data/valid_nounk.txt $DATA_CACHE
 rsync -avhW --no-compress --progress /work/paras/coderep/DeepTyper/data/train_nounk.txt $DATA_CACHE
+rsync -avhW --no-compress --progress /work/paras/coderep/DeepTyper/data/test_projects_gold_filtered.json $DATA_CACHE
 
 # set up experiment dependencies
 cd /work/paras/representjs
@@ -66,16 +57,13 @@ pip install torch
 pip install -e .
 npm install
 
-python representjs/type_prediction.py train --run_name "$RUNNAME-$SLURM_JOB_ID" \
-	--train_filepath $DATA_CACHE/train_nounk.txt \
-    --eval_filepath $DATA_CACHE/valid_nounk.txt \
-    --type_vocab_filepath $DATA_CACHE/target_wl \
-    --spm_filepath $DATA_CACHE/codesearchnet_javascript/csnjs_8k_9995p_unigram_url.model \
-    --subword_regularization_alpha $SUBWORD_REGULARIZATION \
-	--num_workers 8 \
+python representjs/type_prediction.py eval \
+  --eval_filepath "$DATA_CACHE/test_projects_gold_filtered.json" \
+	--type_vocab_filepath $DATA_CACHE/target_wl \
+	--spm_filepath $DATA_CACHE/codesearchnet_javascript/csnjs_8k_9995p_unigram_url.model \
+	--resume_path "$CKPT" \
+  --subword_regularization_alpha $SUBWORD_REGULARIZATION \
+	--num_workers 4 \
 	--batch_size $BATCHSIZE \
-	--max_seq_len $MAXSEQLEN \
-	--max_eval_seq_len 2048 \
-	--pretrain_resume_path "$CKPT" \
-	--pretrain_resume_encoder_name $RESUME_ENCODER_NAME \
-	--lr $LR
+	--max_seq_len -1
+
